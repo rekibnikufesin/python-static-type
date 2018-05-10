066aec119fbc27d1748e8ef2235e8d35ba8f661d
Will Button <will@willbutton.co>
2018-05-09 19:19:43 -0700

    Initial commit

---
+++ type_checking.py
@@ -0,0 +1,21 @@
+from typing import Dict, List
+
+
+def add_num(a: int, b: int):
+    return a + b
+
+
+print(add_num("foo", 2))
+
+my_string: str
+
+my_bool: bool
+
+
+my_nums: List[int]
+
+my_dict: Dict[str, int] = {
+    "Patriots": 23,
+    "Falcons": 7
+}
+
